9218ed14a614811f79f061e2c69cc1abe4b9ae33
krlk4k <fhds.hdd@mail.ru>
2021-02-21 10:06:01 +0300

    add free in 5 laba

---
+++ cmake-build-debug/CMakeFiles/laba5.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/fgrisell/CLionProjects/Data-Structures-and-Algorithms/LabaratoryWork/Part1/Topic3/ex03(laba5)/list.cpp" "/Users/fgrisell/CLionProjects/Data-Structures-and-Algorithms/cmake-build-debug/CMakeFiles/laba5.dir/LabaratoryWork/Part1/Topic3/ex03(laba5)/list.cpp.o"
+  "/Users/fgrisell/CLionProjects/Data-Structures-and-Algorithms/LabaratoryWork/Part1/Topic3/ex05(laba6)/dynamic_list.cpp" "/Users/fgrisell/CLionProjects/Data-Structures-and-Algorithms/cmake-build-debug/CMakeFiles/laba5.dir/LabaratoryWork/Part1/Topic3/ex05(laba6)/dynamic_list.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././LabaratoryWork/Part1/Topic2/ex02"
+  ".././LabaratoryWork/Part1/Topic2/ex06"
+  ".././LabaratoryWork/Part1/Topic2/ex07"
+  "../("
+  "../laba3"
+  "../)"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/laba5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba5.dir/LabaratoryWork/Part1/Topic3/ex03(laba5)/list.cpp.o"
+  "CMakeFiles/laba5.dir/LabaratoryWork/Part1/Topic3/ex05(laba6)/dynamic_list.cpp.o"
+  "laba5"
+  "laba5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
